99cb01aca4a9b3ed5a8466f31c4c8cc19e7b5bea
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-24 18:55:28 +1000

    Basic homescreen view to test link and group view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import LinkGroupView from "@/components/LinkGroupView";
+import Link from "classes/Link";
+import LinkGroup, { ILinkGroup } from "classes/LinkGroup";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+
+  const link1 = new Link({
+    name:'GitHub',
+    url:'https://github.com',
+    favicon:''
+})
+const link2 = new Link({
+    name:'Bitbucket',
+    url:'https://bitbucket.com',
+    favicon:''
+})
+const links = [link1, link2]
+const args: ILinkGroup = {
+    name:"My link group",
+    links
+}
+const group = new LinkGroup(args)
+
+  return <div><LinkGroupView group={group} /></div>;
 }
