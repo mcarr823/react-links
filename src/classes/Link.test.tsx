c30206ed09b1a7a1a2fe1ffd190723f4410907b1
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-24 18:06:59 +1000

    Added an object representing a link on the web page, plus some unit tests

---
+++ src/classes/Link.test.tsx
@@ -0,0 +1,41 @@
+import Link, { ILink } from "./Link"
+
+/**
+ * If a favicon is not defined, then getFavicon() should return
+ * an empty string.
+ */
+test("Empty favicon", () => {
+    
+    const args: ILink = {
+        name:'GitHub',
+        url:'https://github.com',
+        favicon:''
+    }
+    const l = new Link(args)
+
+    expect(l.name).toBe(args.name)
+    expect(l.url).toBe(args.url)
+    expect(l.favicon).toBe(args.favicon)
+    expect(l.getFavicon()).toBe("")
+
+})
+
+/**
+ * If a favicon is defined, then it should be prepended with a
+ * file path.
+ */
+test("Actual favicon", () => {
+    
+    const args: ILink = {
+        name:'GitHub',
+        url:'https://github.com',
+        favicon:'favicon.ico'
+    }
+    const l = new Link(args)
+
+    expect(l.name).toBe(args.name)
+    expect(l.url).toBe(args.url)
+    expect(l.favicon).toBe(args.favicon)
+    expect(l.getFavicon()).toBe("/data/favicons/favicon.ico")
+
+})
---
+++ src/classes/Link.tsx
@@ -0,0 +1,44 @@
+/**
+ * Represents a URL displayed on the website.
+ */
+export default class Link {
+    
+    name: string;
+    url: string;
+    favicon: string;
+
+    /**
+     * @param name Name of the website. eg. GitHub
+     * @param url URL of the website. eg. https://github.com
+     * @param favicon Name of the favicon, if one has been downloaded.
+     * Otherwise it should be an empty string.
+     */
+    constructor(args: ILink){
+        this.name = args.name
+        this.url = args.url
+        this.favicon = args.favicon
+    }
+
+    /**
+     * Get the filesystem path of the favicon.
+     * 
+     * @returns Path to the favicon, or an empty string if one
+     * hasn't been downloaded yet.
+     */
+    getFavicon(){
+        
+        if (this.favicon.length === 0){
+            return ""
+        }
+        
+        return `/data/favicons/${this.favicon}`
+
+    }
+
+}
+
+export interface ILink {
+    name: string;
+    url: string;
+    favicon: string;
+}
