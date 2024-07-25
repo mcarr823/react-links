036b5bf753112e8f45623131eb60b38e721e9986
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-25 19:12:23 +1000

    Updated home page to use new viewmodel and api endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,18 @@
+"use client"
+
 import LinkGroupView from "@/components/LinkGroupView";
 import Link from "classes/Link";
 import LinkGroup, { ILinkGroup } from "classes/LinkGroup";
-
-export const metadata = {
-  title: "App Router",
-};
+import HomeViewModel from "viewmodels/HomeViewModel";
 
 export default function Page() {
 
-  const link1 = new Link({
-    name:'GitHub',
-    url:'https://github.com',
-    favicon:''
-})
-const link2 = new Link({
-    name:'Bitbucket',
-    url:'https://bitbucket.com',
-    favicon:''
-})
-const links = [link1, link2]
-const args: ILinkGroup = {
-    name:"My link group",
-    links
-}
-const group = new LinkGroup(args)
+  const model = HomeViewModel()
 
-  return <div><LinkGroupView group={group} /></div>;
+  const linkGroups = model.groups.map(g => (<LinkGroupView group={g} />))
+
+  return (
+    <div className="container">{linkGroups}</div>
+  )
+
 }
