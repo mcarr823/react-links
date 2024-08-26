8df1005c700693e5f3a8d87a13091ab51ef38417
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-08-26 18:46:47 +1000

    Started adding unit tests for home page

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -3,8 +3,52 @@
  */
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
+import HomeViewModel, { IHomeViewModel } from "viewmodels/HomeViewModel";
+import LinkGroup, { ILinkGroup } from "classes/LinkGroup";
+import Link from "classes/Link";
 
-it("App Router: Works with Server Components", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("App Router");
+it("Home page", () => {
+
+  const id = 123
+  const name = "test name"
+  const links: Array<Link> = [
+      new Link({
+          name:'link1',
+          url:'url1',
+          favicon:'favicon1'
+      }),
+      new Link({
+          name:'link2',
+          url:'url2',
+          favicon:'favicon2'
+      }),
+  ]
+  const linkGroups: ILinkGroup = { id, name, links }
+
+  const groups = [
+    new LinkGroup(linkGroups)
+  ]
+  const addGroup = () => {
+    groups.push(new LinkGroup({ id:0, name:'', links:[] }))
+  }
+  const removeGroup = (i: number) => {}
+  const updateGroup = (i: number, linkGroup: LinkGroup) => {}
+  const addLink = (i: number) => {}
+  const removeLink = (group: number, link: number) => {}
+  const model: IHomeViewModel = {
+    groups,
+    addGroup,
+    removeGroup,
+    updateGroup,
+    addLink,
+    removeLink
+  }
+
+  render(<Page model={model}/>);
+  expect(screen.getByRole("heading")).toHaveTextContent("Links");
+
+  expect(groups.length).toBe(1)
+  screen.getByRole("addLinkButton").click()
+  expect(groups.length).toBe(2)
+  
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,13 +23,14 @@ export default function Page({
 
   return (
     <div className="ms-3 me-3 p-3">
-      <h2 className="p-3">Links</h2>
+      <h2 className="p-3" role="heading">Links</h2>
       <button
         className="btn btn-outline-success"
         onClick={model.addGroup}
         title="Add New Link Group"
+        role="addLinkButton"
       ><Plus/> New Link Group</button>
-      <div className="row mt-3">
+      <div className="row mt-3" role="linkGroups">
         {linkGroups}
       </div>
     </div>
