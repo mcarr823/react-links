747c94f81696a7436b931bd9bc42b72ff1f25d54
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-09-28 09:33:18 +1000

    Split linkview tests into two classes, now that the editlinkview is a separate component

---
+++ src/components/EditLinkView.test.tsx
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import Link from "classes/Link";
+import EditLinkView from "./EditLinkView";
+
+it("EditLinkView component", () => {
+
+    let removeButtonClicked = 0
+
+    const link = new Link({
+        name:'link1',
+        url:'url1',
+        favicon:'favicon1'
+    })
+    const removeLink = () => {
+        removeButtonClicked++
+    }
+
+    render(<EditLinkView link={link} removeLink={removeLink}/>);
+
+    expect(screen.getByRole("editLinkViewName")).toHaveValue(link.name);
+    expect(screen.getByRole("editLinkViewUrl")).toHaveValue(link.url);
+
+    expect(removeButtonClicked).toBe(0)
+    screen.getByRole("removeLinkButton").click()
+    expect(removeButtonClicked).toBe(1)
+  
+});
