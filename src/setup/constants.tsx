15be7160b93731c8dd0732688e12388f018b9240
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-05 08:33:50 +1000

    Updated the data storage location so that running unit tests won't overwrite the real data file if both production and testing environments are run from the same location

--- src/setup/constants.tsx
+++ src/setup/constants.tsx
@@ -5,11 +5,24 @@
  */
 
 /**
- * Defined where the JSON file used for storing the user's
+ * Defines where the JSON file used for storing the user's
  * links should reside.
+ * 
+ * The exact path will vary depending on whether we're running
+ * live, or from a unit test.
  * 
  * Note: this was moved out of the API endpoint classes because
  * those classes have strict rules about what they are allowed
  * to export in order to adhere to the NextJS schema.
  */
-export const dataFile = "data/links.json"
+export const dataFile = isJest() ? "data/jest.json" : "data/links.json"
+
+/**
+ * Checks if the current process is running from Jest or not.
+ * ie. If we're in the middle of running a unit test.
+ * 
+ * @returns true if this process is running from Jest
+ */
+function isJest(): Boolean {
+    return process.env.JEST_WORKER_ID !== undefined
+}
