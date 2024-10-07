a99822f3b07f607edc0a82e90b2c34cd4977ba22
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-07 14:01:56 +1100

    Added an enum for known import and export formats

---
+++ src/enums/DataFormat.tsx
@@ -0,0 +1,8 @@
+/**
+ * Formats for importing/exporting data from the system.
+ * These are used on the import/export pages and API paths.
+ */
+export enum DataFormat{
+	LINKS = "links",
+	ONETAB = "onetab"
+}
