58442c4295b5919788603a75111727acc8bcd9df
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-07 14:46:31 +1100

    Updated the import text route and unit test for the links data format so that it appends new links instead of overwriting the old ones, and it avoids duplicate IDs

--- src/app/api/import/links/text/route.test.tsx
+++ src/app/api/import/links/text/route.test.tsx
@@ -2,14 +2,13 @@
  * @jest-environment node
  */
 
-import LinkGroup from "classes/LinkGroup"
 import ILinkGroup from "interfaces/ILinkGroup"
 import { PUT } from "./route"
 import { writeFile, readFile } from "node:fs/promises"
-import Link from "classes/Link"
 import IImportLinksTextRequest from "interfaces/IImportLinksTextRequest"
 import { dataFile } from "setup/constants"
 import ILink from "interfaces/ILink"
+import { DataFormat } from "enums/DataFormat"
 
 test("PUT - invalid request", async () => {
 
@@ -34,7 +33,10 @@ test("PUT - valid request", async () => {
     const name = 'test'
     const links: Array<ILink> = []
     const linkGroup: ILinkGroup = { id, name, links }
-    const body: IImportLinksTextRequest = { text: JSON.stringify([linkGroup]) }
+    const body: IImportLinksTextRequest = {
+        format: DataFormat.LINKS,
+        text: JSON.stringify([linkGroup])
+    }
     const requestObj = {
         json: async () => (body),
     } as any;
@@ -42,11 +44,58 @@ test("PUT - valid request", async () => {
 
     const buf = await readFile(dataFile)
     const str = buf.toString()
-    const json: [ILinkGroup] = JSON.parse(str)
+    const json: ILinkGroup[] = JSON.parse(str)
 
     expect(json.length).toBe(1)
     expect(json[0].id).toBe(id)
     expect(json[0].name).toBe(name)
     expect(json[0].links.length).toBe(links.length)
+
+})
+
+/**
+ * This test is the same as the one above, but it also
+ * performs a second PUT request afterwards to check if
+ * the ID from the second request was automatically adjusted
+ * to avoid duplication.
+ */
+test("PUT - ID check", async () => {
+    
+    const output = JSON.stringify([])
+    await writeFile(dataFile, output)
+    
+    const id = 1
+    const name = 'test'
+    const links: Array<ILink> = []
+    const linkGroup: ILinkGroup = { id, name, links }
+    const body: IImportLinksTextRequest = {
+        format: DataFormat.LINKS,
+        text: JSON.stringify([linkGroup])
+    }
+    const requestObj = {
+        json: async () => (body),
+    } as any;
+    await PUT(requestObj)
+
+    // Run the request a second time
+    await PUT(requestObj)
+
+    const buf = await readFile(dataFile)
+    const str = buf.toString()
+    const json: ILinkGroup[] = JSON.parse(str)
+
+    // We ran the request twice, so there should be 2 entries
+    expect(json.length).toBe(2)
+
+    // The first entry should match the initial PUT
+    expect(json[0].id).toBe(id)
+    expect(json[0].name).toBe(name)
+    expect(json[0].links.length).toBe(links.length)
+
+    // The second entry should be the same, but with the
+    // ID incremented by 1 automatically to avoid duplication
+    expect(json[1].id).toBe(id+1)
+    expect(json[1].name).toBe(name)
+    expect(json[1].links.length).toBe(links.length)
 
 })
--- src/app/api/import/links/text/route.tsx
+++ src/app/api/import/links/text/route.tsx
@@ -3,18 +3,49 @@ import IImportLinksTextRequest from "interfaces/IImportLinksTextRequest"
 import ILinkGroup from "interfaces/ILinkGroup"
 import { dataFile } from "setup/constants"
 import { writeFile } from "node:fs/promises"
+import { GET as getLinks } from "../../../links/route"
 
-// Inserts a new row
+/**
+ * PUT request for the /api/import/links/text/ route.
+ * 
+ * Imports a Links JSON file, passed as a string, and adds
+ * any parsed LinkGroup objects into `dataFile`.
+ * 
+ * @param request HTTP request containing IImportLinksTextRequest
+ * JSON data.
+ * @returns Either an empty JSON object, or an error
+ */
 export async function PUT(request: Request) {
 
     const json: IImportLinksTextRequest = await request.json()
     const text = json.text
-    try {
-        const iLinkGroups: Array<ILinkGroup> = JSON.parse(text)
-        const linkGroups = iLinkGroups.map(g => new LinkGroup(g))
-                                        .filter(g => g.validate())
 
-        const output = JSON.stringify(linkGroups)
+    try {
+
+        // Start by parsing the new entries.
+        // If any of them are invalid, throw an error.
+        const iLinkGroups: Array<ILinkGroup> = JSON.parse(text)
+        iLinkGroups.forEach(g => {
+            const lg = new LinkGroup(g)
+            if (!lg.validate()) throw new Error("Invalid linkgroup")
+        })
+
+        // Then grab the existing entries
+        const oldLinkGroups: Array<ILinkGroup> = await getLinks(request).then(r => r.json())
+        const oldIds = oldLinkGroups.map(g => g.id)
+        const biggestId = oldIds.length > 0 ? Math.max(...oldIds) : 0
+
+        // Adjust the IDs of the new entries so that they don't
+        // overwrite the IDs of the old entries.
+        // We don't want any duplicate IDs.
+        iLinkGroups.forEach((g, index) => {
+            g.id = biggestId + index + 1
+            oldLinkGroups.push(g)
+        })
+
+        // Finally, write all of the entries, both old and new, into
+        // the JSON file together.
+        const output = JSON.stringify(oldLinkGroups)
         await writeFile(dataFile, output)
     
         return Response.json({})
