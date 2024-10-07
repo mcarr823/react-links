cfd216ba375db431a3766a1ccf564e0ed0234d20
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-10-07 15:04:52 +1100

    Missing interface field

--- src/interfaces/IImportLinksTextRequest.tsx
+++ src/interfaces/IImportLinksTextRequest.tsx
@@ -1,3 +1,23 @@
+/**
+ * Interface defining a text import into the Links program.
+ */
 export default interface IImportLinksTextRequest{
+
+    /**
+     * Format of the input data.
+     * 
+     * @see DataFormat
+     */
+    format: string;
+
+    /**
+     * The input data.
+     * 
+     * This should be a string, regardless of the original export format.
+     * 
+     * eg. JSON should be turned into a JSON string and then placed
+     * in this field, rather than a JSON object being transmitted directly.
+     */
     text: string;
+
 }
